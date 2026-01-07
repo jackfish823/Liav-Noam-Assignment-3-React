9b5ab511bfc49ca99f2d5309e3abcc53150173f6
noamguy <noam.guy09@gmail.com>
2026-01-07 14:45:38 +0200

    move outside calculate winner

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,32 +1,32 @@
 import { useState } from 'react';
 import GameIcon from './GameIcon';
 
+const calculateWinner = (squares: (string | null)[]) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
+
 const TicTacToe = () => {
   const [board, setBoard] = useState<(string | null)[]>(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(true);
-
-  const calculateWinner = (squares: (string | null)[]) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-
-    return null;
-  };
 
   const winner = calculateWinner(board);
   const isDraw = !winner && board.every((square) => square !== null);
